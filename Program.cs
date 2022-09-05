7f37173c13a1b05e25ca73385628122b6d7f0dfd
LukashevichEvgeniy <humansyndr8me@gmail.com>
2022-09-06 00:41:04 +0800

    create array method is done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
 ﻿
+string[] CreateUserArray(int size)
+{
+    string[] array = new string[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Input the {i + 1} string of {size}: ");
+        array[i] = Console.ReadLine();
+    }
+
+    Console.WriteLine();
+    return array;
+}
+
+
